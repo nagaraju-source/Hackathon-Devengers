d05ddb252f324d2df3a50b14858423cdfe952e97
Nagaraju Meruva <nagaraju.meruva@hitachiconsulting.com>
2020-01-18 23:25:37 +0530

    seeler-service

---
+++ seller-service/src/main/java/com/devengers/hacknight6/spider/b2bpayments/controller/SellerController.java
@@ -0,0 +1,10 @@
+package com.devengers.hacknight6.spider.b2bpayments.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SellerController {
+
+
+}
---
+++ seller-service/src/main/java/com/devengers/hacknight6/spider/b2bpayments/controller/modal/Product.java
@@ -0,0 +1,8 @@
+package com.devengers.hacknight6.spider.b2bpayments.controller.modal;
+
+
+public class Product {
+
+    private int productID;
+
+}
